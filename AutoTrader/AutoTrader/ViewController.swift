39c63d8efa58091abc7a05733fce4f821e195621
An Bảo <anbao@Ans-MacBook-Pro.local>
2024-04-25 20:09:43 +0700

    Initial Project

---
+++ AutoTrader/AutoTrader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoTrader
+//
+//  Created by An Bảo on 25/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
